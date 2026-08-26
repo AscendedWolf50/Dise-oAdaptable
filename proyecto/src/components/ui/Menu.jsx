1f094b196123ef0599265bb77626d4f6f52b1ae6
AscendedWolf50 <110170642+AscendedWolf50@users.noreply.github.com>
2026-08-26 18:23:41 -0500

    Ajustes para el despliegue

--- proyecto/src/components/ui/Menu.jsx
+++ proyecto/src/components/ui/Menu.jsx
@@ -5,16 +5,16 @@ export default function Menu() {
       background: '#111', justifyContent: 'center', borderBottom: '3px solid #aa3bff',
       marginBottom: '20px'
     }}>
-      <a href="/" style={{ color: 'white', fontWeight: 'bold' }}>Inicio</a>
-      <a href="/login" style={{ color: 'white', fontWeight: 'bold' }}>Registro</a>
-      <a href="/niveles" style={{ color: 'white', fontWeight: 'bold' }}>Niveles</a>
-      <a href="/perfil" style={{ color: 'white', fontWeight: 'bold' }}>Perfil</a>
-      <a href="/configuracion" style={{ color: 'white', fontWeight: 'bold' }}>Configuración</a>
-      <a href="/tutorial" style={{ color: 'white', fontWeight: 'bold' }}>Tutorial</a>
-      <a href="/gameplay" style={{ color: 'white', fontWeight: 'bold' }}>Gameplay</a>
-      <a href="/pausa" style={{ color: 'white', fontWeight: 'bold' }}>Pausa</a>
-      <a href="/victoria" style={{ color: 'white', fontWeight: 'bold' }}>Victoria</a>
-      <a href="/derrota" style={{ color: 'white', fontWeight: 'bold' }}>Derrota</a>
+      <a href="#/" style={{ color: 'white', fontWeight: 'bold' }}>Inicio</a>
+      <a href="#/login" style={{ color: 'white', fontWeight: 'bold' }}>Registro</a>
+      <a href="#/niveles" style={{ color: 'white', fontWeight: 'bold' }}>Niveles</a>
+      <a href="#/perfil" style={{ color: 'white', fontWeight: 'bold' }}>Perfil</a>
+      <a href="#/configuracion" style={{ color: 'white', fontWeight: 'bold' }}>Configuración</a>
+      <a href="#/tutorial" style={{ color: 'white', fontWeight: 'bold' }}>Tutorial</a>
+      <a href="#/gameplay" style={{ color: 'white', fontWeight: 'bold' }}>Gameplay</a>
+      <a href="#/pausa" style={{ color: 'white', fontWeight: 'bold' }}>Pausa</a>
+      <a href="#/victoria" style={{ color: 'white', fontWeight: 'bold' }}>Victoria</a>
+      <a href="#/derrota" style={{ color: 'white', fontWeight: 'bold' }}>Derrota</a>
     </nav>
   );
 }
--- proyecto/src/main.jsx
+++ proyecto/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import './App.css'
 
@@ -20,7 +20,7 @@ import Menu from './components/ui/Menu.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Menu />
       <Routes>
         <Route path="/" element={<Inicio />} />
@@ -34,6 +34,6 @@ createRoot(document.getElementById('root')).render(
         <Route path="/victoria" element={<Victoria />} />
         <Route path="/derrota" element={<Derrota />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
